d2f092b18c2791c1eb68262f40a21d81388cbb05
tigerbluejay <josemariairiarte@gmail.com>
2022-08-16 19:33:11 -0300

    Project Commit

---
+++ BankAccountSolution/BankAccount/Program.cs
@@ -0,0 +1,38 @@
+﻿// See https://aka.ms/new-console-template for more information
+using BankAccount.Classes; 
+    
+var account = new Account("<name>", 1000);
+Console.WriteLine($"Account {account.Number} was created for {account.Owner} " +
+    $"with {account.Balance} initial balance.");
+
+account.MakeWithdrawal(500, DateTime.Now, "Rent Payment");
+Console.WriteLine(account.Balance);
+account.MakeDeposit(100, DateTime.Now, "Friend paid me back");
+Console.WriteLine(account.Balance);
+
+Console.WriteLine(account.GetAccountHistory());
+
+// Test for negative balance
+try
+{
+    account.MakeWithdrawal(750, DateTime.Now, "Attempt to overdraw");
+} catch (InvalidOperationException e)
+{
+    Console.WriteLine("Exception caught trying to overdraw");
+    Console.WriteLine(e.ToString());
+}
+
+
+// Test that the initial balances must be positive
+Account invalidAccount;
+try
+{
+    invalidAccount = new Account("invalid", -55);
+} catch (ArgumentOutOfRangeException e)
+{
+    Console.WriteLine("Exception caught creating account with negative balance");
+    Console.WriteLine(e.ToString());
+    return;
+}
+
+
